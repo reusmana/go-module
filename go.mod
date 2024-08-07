b590c93530741277309271ce40e40ccf5758740c
reusmana <reusmanasujani@gmail.com>
2024-08-08 04:50:25 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/reusmana/go-module
+
+go 1.22.6
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import fmt "Fmt"
+
+
+func SayHello(name string) string {
+	return "Hello " + name
+}
+
+func main(){
+	fmt.Println(SayHello("reusmana"))
+}
+
